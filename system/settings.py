962df0aaebba8dd1ed7aa735e9bc6bb226ee4da0
Jevgenijus Vasiljevas <jevgen.vasiljevas@gmail.com>
2024-06-13 17:17:27 +0300

    Deploy to Heroku

--- system/settings.py
+++ system/settings.py
@@ -26,7 +26,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'blog.apps.BlogConfig',
     'tinymce',
     'debug_toolbar',
